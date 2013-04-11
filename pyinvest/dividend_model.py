7601aebbaba5a7190590579528305342dd84549c
James Whinfrey <james@conceptric.co.uk>
2013-04-11 17:17:01 +0100

    Adds a steady state Gordon Growth Model based function that can handle lists of costs of equity and growth rates for sensitivity analysis

--- pyinvest/dividend_model.py
+++ pyinvest/dividend_model.py
@@ -12,3 +12,35 @@ def get_projected_dividends(current, rate, years=1, places=4):
     Returns a list of the compounded expected dividends.
     '''
     return current * get_discounting_factors(rate, years, places)
+
+def get_growth_rate(eps, dps, roe):
+    '''
+    Calculates the expected growth rate based on:
+    
+    eps: earnings per share.
+    dps: dividends per share.
+    roe: fractional return on equity.
+    
+    Returns the fractional expected growth rate.
+    '''
+    return (eps / dps) * roe
+    
+def gordon_growth(current, coe, growth):
+    '''
+    Calculates the steady state price of the stock 
+    based on dividends.
+    
+    current : current dividend.
+    coe     : cost of equity (can be a list of values).
+    growth  : expected dividend growth rate 
+            (can be a list of values and the base line is the 
+            first in the list).
+    
+    Returns a list of the values of the stock based on the 
+    supplied data.
+    '''
+    try:
+        future = get_projected_dividends(current, growth[0])
+    except TypeError:
+        future = get_projected_dividends(current, growth)
+    return selective_round(future / (np.array(coe) - np.array(growth)), 2)
--- pyinvest/tests/dividend_model_test.py
+++ pyinvest/tests/dividend_model_test.py
@@ -12,4 +12,54 @@ class TestProjectedDividends(unittest.TestCase):
         
     def test_returns_next_two_years_dividends(self):
         actual = get_projected_dividends(1, 0.05, 2)
-        self.assertEquals(actual, [1.05, 1.1025])    
+        self.assertEquals(actual, [1.05, 1.1025]) 
+        
+class TestDividendGrowthRate(unittest.TestCase):
+    """
+    Test calculating the dividend growth rate.
+    """
+    def test_get_growth_rate(self):
+        actual = get_growth_rate(2, 1, 0.1)
+        self.assertEquals(actual, 0.2)
+
+class TestGordonGrowthModel(unittest.TestCase):
+    """
+    Test valuation using the Gordon Growth Model.
+    """
+    def test_gordon_growth(self):
+        dividend = 1
+        cost_of_equity = 0.1
+        growth_rate = 0.05
+        actual = gordon_growth(dividend, cost_of_equity, growth_rate)
+        self.assertEquals(actual[0], 21)
+
+    def test_gordon_growth_with_list_coe(self):
+        ''' Returns a list of valuations given a list of costs of equity '''
+        dividend = 1
+        cost_of_equity = [0.08, 0.1, 0.12]
+        growth_rate = 0.05
+        actual = gordon_growth(dividend, cost_of_equity, growth_rate)
+        self.assertEqual(len(actual), 3)
+        self.assertEquals(actual[0], 35)
+        self.assertEquals(actual[1], 21)
+        self.assertEquals(actual[2], 15)
+
+    def test_gordon_growth_with_list_growth_rates(self):
+        ''' Returns a list of valuations given a list of growth rates '''
+        dividend = 1
+        cost_of_equity = 0.15
+        growth_rate = [0.05, 0.075, 0.1]
+        actual = gordon_growth(dividend, cost_of_equity, growth_rate)
+        self.assertEquals(actual[0], 10.5)
+        self.assertEquals(actual[1], 14)
+        self.assertEquals(actual[2], 21)
+
+    def test_gordon_growth_with_lists_for_both(self):
+        ''' Returns a list of valuations given lists of coe and growth rates '''
+        dividend = 1
+        cost_of_equity = [0.1, 0.15, 0.2]
+        growth_rate = [0.05, 0.1, 0.15]
+        actual = gordon_growth(dividend, cost_of_equity, growth_rate)
+        self.assertEqual(len(actual), 3)
+        for value in actual:
+            self.assertEquals(value, 21)
