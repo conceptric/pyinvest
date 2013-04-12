64a39b9900388f28099700999acc9584d0fd8f36
James Whinfrey <james@conceptric.co.uk>
2013-04-12 11:29:58 +0100

    Adds the dividend_model module to the pyinvest manifest

--- pyinvest/__init__.py
+++ pyinvest/__init__.py
@@ -1,6 +1,8 @@
 '''
 Package containing tools for investment valuation.
 
-utils: A module of general purpose valuation functions.
+utils           : A module of general purpose valuation functions.
+dividend_model  : A module for dividend growth model valuations.
 '''
 from utils import *
+from dividend_model import *
